ac80c722c7ededc9cc83f177cef963d4c4068868
max <max@maxs-MacBook-Pro.local>
2018-01-19 10:27:18 -0500

    filling out app structure

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+	recipes: Recipe[] =[
+		new Recipe ('orange pie', 'tangy pie with orange', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Peach_mango_pie_%2813978765798%29.jpg/600px-Peach_mango_pie_%2813978765798%29.jpg'),
+		new Recipe ('pumpkin pie', 'on the pie theme', 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Pumpkin_Pie_from_a_%2Areal%2A_pumpkin%2C_November_2007.jpg')
+	];
 
-  constructor() { }
+	constructor() { }
 
   ngOnInit() {
   }
