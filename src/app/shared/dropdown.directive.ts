83a0711485a40d4eba236965153125de8820db98
max <max@maxs-MacBook-Pro.local>
2018-01-23 06:30:18 -0500

    added dropdown directive

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, OnInit, HostListener, HostBinding} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+	@HostBinding('class.open') isOpen = false;
+
+
+  @HostListener('click') toggleOpen(eventData: Event) {
+  	this.isOpen = !this.isOpen;
+  }
+
+}
