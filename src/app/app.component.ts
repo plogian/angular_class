c0f19c2561169389b528aae7141b42ec73629e0b
plogian <lillianpodlog@gmail.com>
2018-02-07 12:50:26 -0500

    changed signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,13 @@ import * as firebase from 'firebase';
 })
 export class AppComponent implements OnInit {
 	showPage = "Recipes";
+	apiKey='';
+	authDomain='';
 
 	ngOnInit() {
 		firebase.initializeApp({
-			apiKey: "AIzaSyBviJ0_8jRtTZE1KStwjP46h320FEtalE4",
-			authDomain: "recipe-book-4dfe0.firebaseapp.com"
+			apiKey: this.apiKey,
+			authDomain: this.authDomain
 		});
 	}
 
