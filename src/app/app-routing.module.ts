f3e8ac87c1761cb2b61cfacd58c502f52e21d493
plogian <lillianpodlog@gmail.com>
2018-02-08 15:32:02 -0500

    modularized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,18 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules} from '@angular/router';
 
-import { Routes, RouterModule } from '@angular/router';
-
+import { HomeComponent } from './core/home/home.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
-import { AuthGuardService } from './auth/auth-guard.service';
 
 const appRoutes: Routes = [
-	{path: '', redirectTo: '/recipes', pathMatch: 'full'},
-	{path: 'shopping', component: ShoppingListComponent},
-	{path: 'recipes', component: RecipesComponent, children: [
-		{path: '', component: RecipeStartComponent},
-		{path: 'new', component: RecipeEditComponent, canActivate: [AuthGuardService]},
-		{path: ':id', component: RecipeDetailComponent},
-		{path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuardService]}
-	]}, 
-	{path: 'signup', component: SignupComponent},
-	{path: 'signin', component: SigninComponent},
-	{path: '**', redirectTo: '/shopping'}
+	{path: '', component: HomeComponent},
+	{path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'}, //lazy loading
+	{path: 'shopping', component: ShoppingListComponent}
 ];
 
 @NgModule({
 	imports: [
-	RouterModule.forRoot(appRoutes)
+		RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})
 	], 
 	exports: [RouterModule]
 })
